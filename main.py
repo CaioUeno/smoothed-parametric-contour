8b04e90a577d2197de6639e8f3efdb2091ee13ec
leite <leite@pop-os.localdomain>
2020-10-12 19:35:18 -0300

    incrementing main and plotting figures

--- main.py
+++ main.py
@@ -2,13 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from objects import *
 from gaussian_smooth import *
+# import image_contour
 
-img = cross() # can change later
+fig_type = input("Select a figure: Cross, Square, Triangle, Circle or Random: ").lower()
+if fig_type == "cross":
+    img = cross()
+elif fig_type == "square":
+    img = square()
+elif fig_type == "triangle":
+    img = triangle()
+elif fig_type == "circle":
+    img = circle()
+elif fig_type == "random":
+    img = random()
+else:
+    raise Exception("Wrong Input Type")
+
+plt.imshow(img)
+plt.savefig(f"images/{fig_type}.png")
 
 # get contour
-
-contour_x = # to complete
-contour_y = # to complete
+# contour_x = # to complete
+# contour_y = # to complete
 
 filter_size = 5
 sigma = 0.5
